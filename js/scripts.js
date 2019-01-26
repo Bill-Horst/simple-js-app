1ffdd3bd7697a89231a01dabce37d15c3847a871
Bill Horst <bill@billhorst.com>
2019-01-26 11:18:20 -0700

    1.3 changes - added pokemon repository and printed name and info to document

--- js/scripts.js
+++ js/scripts.js
@@ -1,1 +1,26 @@
-alert('Hello world');
+var repository = [
+  {
+    name: 'Bulbasaur',
+    height: '7',
+    types: ['grass', 'poison']
+  },
+  {
+    name: 'Venusaur',
+    height: '2',
+    types: ['monster', 'grass']
+  },
+  {
+    name: 'Squirtle',
+    height: '5',
+    types: ['monster', 'water']
+  }
+];
+
+for (i = 0; i < repository.length; i++) {
+  let pokeName = repository[i].name;
+  let pokeHeight = repository[i].height;
+  document.write('<h1>Name: ' + pokeName + ' (height: ' + pokeHeight + ')</h1>');
+  if (pokeHeight > 4) {
+    document.write('<h3>Wow, that\'s big!</h3>');
+  }
+}
