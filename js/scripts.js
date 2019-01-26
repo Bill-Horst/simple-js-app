dc7f1e4c14ab370a24ff3d4e454bc2e6a1a4f788
Bill Horst <bill@billhorst.com>
2019-01-26 14:12:28 -0700

    1.4 - using iife for pokemon repo

--- js/scripts.js
+++ js/scripts.js
@@ -1,26 +1,52 @@
-var repository = [
-  {
-    name: 'Bulbasaur',
-    height: '7',
-    types: ['grass', 'poison']
-  },
-  {
-    name: 'Venusaur',
-    height: '2',
-    types: ['monster', 'grass']
-  },
-  {
-    name: 'Squirtle',
-    height: '5',
-    types: ['monster', 'water']
-  }
-];
+(function() {
 
-for (i = 0; i < repository.length; i++) {
-  let pokeName = repository[i].name;
-  let pokeHeight = repository[i].height;
-  document.write('<h1>Name: ' + pokeName + ' (height: ' + pokeHeight + ')</h1>');
-  if (pokeHeight > 4) {
-    document.write('<h3>Wow, that\'s big!</h3>');
-  }
-}
+  var pokemonRepository = (function() {
+
+    var pokemonRepository = [
+      {
+        name: 'Bulbasaur',
+        height: '7',
+        types: ['grass', 'poison']
+      },
+      {
+        name: 'Venusaur',
+        height: '2',
+        types: ['monster', 'grass']
+      },
+      {
+        name: 'Squirtle',
+        height: '5',
+        types: ['monster', 'water']
+      }
+    ];
+
+    function add(pokemon) {
+      if (typeof pokemon === "object") {
+        pokemonRepository.push(pokemon);
+      } else {
+        return {
+          message: "not an object"
+        }
+      }
+    }
+
+    function getAll() {
+      return pokemonRepository;
+    }
+
+    return {
+      add: add,
+      getAll: getAll
+    }
+  })();
+
+  pokemonRepository.getAll().forEach(function(pokemon) {
+    let pokeName = pokemon.name;
+    let pokeHeight = pokemon.height;
+    document.write('<h1>Name: ' + pokeName + ' (height: ' + pokeHeight + ')</h1>');
+    if (pokeHeight > 4) {
+      document.write('<h3>Wow, that\'s big!</h3>');
+    }
+  })
+
+})();
