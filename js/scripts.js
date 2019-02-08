6cf5f1e1e9249df6ab5e0449638466ad007c1be2
Bill Horst <bill@billhorst.com>
2019-02-08 10:24:32 -0700

    added rough modal functionality

--- js/scripts.js
+++ js/scripts.js
@@ -115,8 +115,73 @@
   // the function run upon clicking a pokemon li
   function showDetails(pokemon) {
     pokemonRepository.loadDetails(pokemon).then( () => {
-      console.log(pokemon);
+      MODAL_CONTROLS.showModal(pokemon);
     });
+  }
+
+})();
+
+const MODAL_CONTROLS = (function() {
+  var $modalContainer = document.querySelector('#modal-container');
+
+  function showModal(pokemon) {
+    console.log(pokemon)
+    // Clear all existing modal content
+    $modalContainer.innerHTML = '';
+
+    var modal = document.createElement('div');
+    modal.classList.add('modal');
+
+    // Add the new modal content
+    var closeButtonElement = document.createElement('button');
+    closeButtonElement.classList.add('modal-close');
+    closeButtonElement.innerText = 'Close';
+    closeButtonElement.addEventListener('click', hideModal);
+
+    var nameDisplay = document.createElement('h1');
+    nameDisplay.innerText = pokemon.name;
+
+    var heightDisplay = document.createElement('p');
+    heightDisplay.innerText = `Height: ${pokemon.height}`;
+
+    var imageDisplay = document.createElement('img');
+    imageDisplay.setAttribute('src', pokemon.imageUrl);
+
+    modal.appendChild(closeButtonElement);
+    modal.appendChild(nameDisplay);
+    modal.appendChild(heightDisplay);
+    modal.appendChild(imageDisplay);
+    $modalContainer.appendChild(modal);
+
+    $modalContainer.classList.add('is-visible');
+  }
+
+  function hideModal() {
+    $modalContainer.classList.remove('is-visible');
+  }
+
+  document.querySelector('#show-modal').addEventListener('click', () => {
+    showModal('Modal title', 'This is the modal content!');
+  });
+
+  window.addEventListener('keydown', (e) => {
+    if (e.key === 'Escape' && $modalContainer.classList.contains('is-visible')) {
+      hideModal();
+    }
+  });
+
+  $modalContainer.addEventListener('click', (e) => {
+    // Since this is also triggered when clicking INSIDE the modal container,
+    // We only want to close if the user clicks directly on the overlay
+    var target = e.target;
+    if (target === $modalContainer) {
+      hideModal();
+    }
+  });
+
+  return {
+    showModal: showModal,
+    hideModal: hideModal
   }
 
 })();
